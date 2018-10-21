f98ae8ff5fc3adf4290bb96172838a451cb58f12
Steven Regis <xcode@CodexRooks-MacBook-Air.local>
2018-10-21 11:49:07 -0400

    adding my files as of now

--- pset1.js
+++ pset1.js
@@ -6,6 +6,14 @@
             find average of all numbers
     @example - average([1,2,3,4]); // 2.5
 */
+const average = arr => {
+    const arrCopy = arr.slice();
+    const total = 0 
+    for (let i = 0; i < arrCopy.length; i++) {
+      total += arrCopy[i]
+      return total / arryCopy.length
+ }
+console.log(average[1, 2, 3, 4])
 
 /*
     @func squareEach
@@ -14,7 +22,38 @@
     @desc - square each value in array
     @example - squareEach([1,2,3,4]); // [1,4,9,16]
 */
+ const squareEach = arr =>{
+     for (let arr = 0; arr < arr.length; i++) {
 
+        Math.pow(arr[i],2);
+    
+    console.log(squareEach[1,2,3,4]));
+ }
+
+
+--------------------------------------
+
+deep copy array 
+ const squareEach = arr =>{
+      cosnt arrCopy = arr.slice(0);
+      const squared = [];
+      for (let i =0; i < arrCopy.length; i++){
+           squared.push (arrCopy[i] ** 2);
+           copyArr[i] = copyArr[i] **2;
+      }
+      return copyArr;
+ }
+
+ console.log (squareEach([1, 2, 3, 4]));
+
+ const squareEach = (arr) =>
+ let resulst =[]
+ for (let i =0; i < arr.length; i++){
+     result.push  (arr[i]  * arr{i});
+
+ }
+
+ return result;
 /*
     @func averageSquare
     @param {array} arr
@@ -28,8 +67,22 @@
         // then find the averate of the array to the right
         averageSquare([1,2,3,4); // 7.5
 */
+  const squareEach = (arr) =>{
+    const copyArr = arr.slice(0);
+    for (let i = 0; i < copyArr.lenght; i++){
+        copyArr{i} =copyArr[i]** 2;
+    }
+    return copyArr;
+  }
 
-/*
+  const averageSquare = (arr) =>{
+      const squaredElements = squareEach(aar);
+      return average(SquaredElements);
+  }
+
+  console.log(averageSquare[1,2,3,4]));
+  
+  /*
     @func negateArr
     @param {array} arr
     @returns {array}
@@ -37,6 +90,16 @@
     @example
         negateArr([1,2,3,4]); // [-1,-2,-3,-4]
 */
+const negateArr = arr =>{
+ const copyArr = [...arr];
+ for (let i =0; i < copyArr.length; i++) {
+     copyArr[i] *= -1;
+    // copyArr[i] = copyArry[i] * -1;
+ }
+ retrun copyArr;
+}
+
+
 
 /*
     @func reverseArr
@@ -45,6 +108,29 @@
     @desc - reverse an array
     @example - reverseArr([1,2,3,4]); // [4,3,2,1]
 */
+ const reverseArr = arr => {
+    let arrCopy = [];
+
+    for (let i = 0; i < arr.length; i++){
+        arrCopy[arr.length - 1 - i ] =arr[i];
+    }
+    return arrCopy:
+ }
+
+ console.log(reverseArr([1, 2, 3, 4]));
+
+// Another example same probllem2
+
+ const reverseArr2 = (arr) => {
+     let arryCopy =[];
+
+     for ( let i = arr.length - 1; i >= 0; i--){
+         arrCopy.push(arr[i]);
+     }
+     return arrCopy;
+ }
+
+// Another example same probllem2
 
 /*
     @func negateBackwards
@@ -54,6 +140,14 @@
     @example
         negateBackwards([1,2,3,4]); // [-4, -3, -2, -1]
 */
+ const negateBackwards = arr =>{
+    le copyArr = []
+    for (let i = arr.legnth - 1; i >= 0; i--){
+        copyArr.push(arr[i] * -1));
+    }
+return copyArr;
+ }
 
+// anotherway 
 
-
+const negatBackwards2 = (arr) => negateArr(reverseArr(arr))
--- pset2.js
+++ pset2.js
@@ -3,8 +3,21 @@
     @param {array} arr
     @returns {array}
     @desc - takes all engative numbers and makes positive
-    @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
-*/
+//     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
+// */
+//  const removeNegatives = (arr) => {
+       
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (arr[i] < 0){
+//             arr[i] *= -1
+//         }
+//     }
+// return arr;
+//  }
+//     console.log(removeNegatives([ -3, -2, 1, 2 , 3, -4])); 
+ 
+
 
 /*
     @func findVal
@@ -19,6 +32,16 @@
         findVal([1,2,3,4], 4); // 3
         findVal([1,2,3,4], 9); // -1
 */
+//  const findVal = (arr, val) =>{
+//       for (let i = 0; i < arr.length; i++){
+//           if ( arr[i] === val ){
+//               return i;
+//           }
+//       } return -1
+//  }
+// console.log(findVal([1,2,3,4], 1));
+// console.log (findVal([1,2,3,4], 4)); // 3
+
 
 /*
     @func removeOdds
@@ -31,29 +54,58 @@
         removeOdds([1,2,3,4]); // [2,4]
         removeOdds([1,"2",3,4]); // [4]
 */
+const removeOdds = (arr) =>{
+    const evenArr =[]
+    for (let i =0; i < arr.length; i++){
+        if (arr[i] % 2 === 0 && typeof arr[i] === "number"){
+            evenArr.push(arr[i])
+        }
 
-/*
-    @func addSquares
-    @param {array} arr
-    @returns {array}
-    @desc - for each value in array, square it then add to array
-    @example - 
-        addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
-*/
-
-/*
-    @func doubleify
-    @param {array} arr
-    @returns {array}
-    @desc - take each value of array, add same value right after
-    @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
-*/
+    }
+    return evenArr;
+}
+console.log(removeOdds([1,2,3,4])); // [2,4]
+console.log(removeOdds([1,"2",3,4])); // [4]
 
 
-/*
-    @func findMax
-    @param {array}
-    @returns {number}
-    @desc - find largest number in array
-    @example - findMax([1,2,3,99,4]); // 99 
-*/
+// /*
+//     @func addSquares
+//     @param {array} arr
+//     @returns {array}
+//     @desc - for each value in array, square it then add to array
+//     @example - 
+//         addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
+// */
+ const addSquares = (arr) =>{
+     const arrCopy = arr.slice();
+     for (let i = 0; i < arr.length; i++){
+       arrCopy.push ** (arr[i]
+        }
+     }
+
+    
+ 
+
+// /*
+//     @func doubleify
+//     @param {array} arr
+//     @returns {array}
+//     @desc - take each value of array, add same value right after
+//     @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
+// */
+
+// const doubleify = (arr) => 
+
+
+// /*
+//     @func findMax
+//     @param {array}
+//     @returns {number}
+//     @desc - find largest number in array
+//     @example - findMax([1,2,3,99,4]); // 99 
+// */
+
+// const findMax = (arr) => {
+
+
+// }r
